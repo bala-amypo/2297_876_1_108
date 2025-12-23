97af5055ea995fb73796f51dec7499bf376affc9
bala-amypo <bala@amypo.in>
2025-12-23 06:44:16 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Supplier Diversity Tracker")
+                        .version("1.0")
+                        .description("API documentation for AmyPO test cases")
+                )
+                .servers(List.of(
+                        new Server().url("https://9126.32procr.amypo.ai/")
+                ));
+    }
+}
