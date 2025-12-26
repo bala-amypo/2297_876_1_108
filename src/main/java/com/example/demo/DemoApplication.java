54d0f14db20b7455543883f3cab201ad1bcbce68
bala-amypo <bala@amypo.in>
2025-12-26 17:29:08 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,3 +1,7 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class DemoApplicationTests {
