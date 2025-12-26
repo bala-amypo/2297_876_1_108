5fc8ac5cc6d1181c9716f819a61beae00813efc2
bala-amypo <bala@amypo.in>
2025-12-26 15:47:41 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DataFetchController {
+public class AuthController {
 
-    @GetMapping("/datafetch")
-    public String dataFetch(){
+    @GetMapping("/Auth")
+    public String Auth(){
         return "AIDS";
     }
 
